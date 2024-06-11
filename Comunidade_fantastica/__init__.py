29ca60989c81afeb3690dd48e16a66f05c9e929d
CamilaBugai <159670999+CamilaBugai@users.noreply.github.com>
2024-06-10 21:09:39 -0300

    Update __init__.py

--- Comunidade_fantastica/__init__.py
+++ Comunidade_fantastica/__init__.py
@@ -1,21 +1,16 @@
-# pip install flask
-# Gerar senha,no terminal,python,import secrets,secrets.token_hex(16)
-#pip install email_validator
-# No formulario html,colocou <form,tem que colocar um token.
-#Criar banco dados: instalar; pip install flask-sqlalchemy
-# pip install flask-bcrypt
-# pip install flask-login
-
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'e0a2b2c9c200a2ad1a3466da64695e18'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.fantastica'
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.fantastica'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
